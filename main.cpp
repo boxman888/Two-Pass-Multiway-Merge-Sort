b0586b7394e45491a1e4d77fafb4085ce85b1f07
boxman888 <epatterson888@hotmail.com>
2020-02-16 17:27:22 -0800

    Stuck on trying to merge the smaller files. the 22 block limit is  the issue. How can you tell if a file is sorted if you can only read 22 blocks at a time.

--- main.cpp
+++ main.cpp
@@ -43,7 +43,7 @@ public:
 		if (dname.size() > (size_t)(floor(40 / sizeof('0'))))
 			this->dname = dname.substr(0, (size_t)floor(40 / sizeof('0')));
 		else
-		this->dname = dname;
+			this->dname = dname;
 	}
 
 	int get_did() {
@@ -100,7 +100,7 @@ public:
 		if (ename.size() > (size_t)floor(40 / sizeof('0')))
 			this->ename = ename.substr(0, (size_t)floor(40 / sizeof('0')));
 		else
-		this->ename = ename;
+			this->ename = ename;
 	}
 
 	int get_eid() {
@@ -127,11 +127,11 @@ private:
 	std::vector<Emp*> emp;
 	std::vector<Dept*> dept;
 
-	 static bool compare_dept(Dept *ls, Dept *rs) {
-		 return ls->get_managerid() < rs->get_managerid();
+	static bool compare_dept(Dept *ls, Dept *rs) {
+		return ls->get_managerid() < rs->get_managerid();
 	}
 
-	static bool compare_emp(Emp *ls, Emp *rs){
+	static bool compare_emp(Emp *ls, Emp *rs) {
 		return ls->get_eid() < rs->get_eid();
 	}
 
@@ -188,11 +188,11 @@ public:
 
 	~File() {
 		if (emp.size() != 0) {
-			for (size_t i = 0; i < emp.size()-1; i++)
+			for (size_t i = 0; i < emp.size() - 1; i++)
 				delete emp[i];
 		}
 		if (dept.size() != 0) {
-			for (size_t i = 0; i < dept.size()-1; i++)
+			for (size_t i = 0; i < dept.size() - 1; i++)
 				delete dept[i];
 		}
 	}
@@ -207,33 +207,31 @@ private:
 
 	int read() {
 		fin.open(table.c_str(), std::ios::in);
+		fin >> std::noskipws;
 
+		double dd = 0;
 		unsigned int count, block_count;
 		std::vector<std::string> row;
 		std::string line, word, temp, filename;
 
 		File* page = NULL;
 		count = block_count = 0;
-		while (fin >> temp) {
-			row.clear();
+		while (std::getline(fin, temp)) {
+ 			row.clear();
 			getline(fin, line);
 			std::stringstream ss(temp);
 
 			while (getline(ss, word, ',')) {
 				row.push_back(word);
 			}
-
-			std::stringstream sd(row[2]);
-			double dd = 0.0;
-			sd >> dd;
-
+			std::stringstream s;
 			if (page == NULL) {
 				count++;
 				filename.clear();
-				sd.str(std::string());
-				sd.clear();
-				sd << count;
-				std::string index = sd.str();
+				s.str(std::string());
+				s.clear();
+				s << count;
+				std::string index = s.str();
 				if (type.compare("dept") == 0)
 					filename = "tempdept_" + index;
 				if (type.compare("emp") == 0)
@@ -241,12 +239,18 @@ private:
 				page = new File(filename);
 			}
 
-			if (type.compare("dept") == 0)
-				page->build_dept(atoi(row[0].c_str()), atoi(row[1].c_str()), dd, row[3]);
+			if (type.compare("dept") == 0) {
 
-			if (type.compare("emp") == 0)
-				page->build_emp(atoi(row[0].c_str()), atoi(row[1].c_str()), dd, row[3]);
-
+				std::cout << row[2] << std::endl;
+				std::stringstream sd(row[2]);
+				sd >> dd;
+				page->build_dept(atoi(row[0].c_str()), atoi(row[3].c_str()), dd, row[1]);
+			}
+			if (type.compare("emp") == 0) {
+				std::stringstream sd(row[3]);
+				sd >> dd;
+				page->build_emp(atoi(row[0].c_str()), atoi(row[1].c_str()), dd, row[1]);
+			}
 			if (block_count == block_size - 1) {
 				page->save();
 				block_count = 0;
@@ -278,7 +282,7 @@ public:
 	}
 };
 
-class BufferHandler{
+class BufferHandler {
 private:
 	std::fstream first_buffer;
 	std::fstream second_buffer;
@@ -287,10 +291,10 @@ private:
 
 	int total;
 	int block_size;
-	
-	void read(std::vector<std::string> &row, std::fstream &name, std::string &temp){
+
+	void read(std::vector<std::string> &row, std::fstream &name, std::string &temp) {
 		std::string line, word, filename;
-		
+
 		getline(name, line);
 		std::stringstream ss(temp);
 
@@ -299,40 +303,82 @@ private:
 		}
 	}
 
+	bool file_compare(std::string file_one, std::string file_two) {
+		if (atoi(file_one.c_str()) < atoi(file_two.c_str()))
+			return true;
+		return false;
+	}
+
+	void addToPage(File *page, std::vector<std::string> row) {
+		double d = 0.0;
+
+		if (type.compare("dept") == 0) {
+			std::stringstream ss(row[2]);
+			ss >> d;
+			page->build_dept(atoi(row[0].c_str()), atoi(row[3].c_str()), d, row[1]);
+		}
+		if (type.compare("emp") == 0) {
+			std::stringstream ss(row[3]);
+			ss >> d;
+			page->build_emp(atoi(row[0].c_str()), atoi(row[1].c_str()), d, row[1]);
+		}
+	}
+
 public:
-	BufferHandler(std::string name, std::string type, int total, int block_size){
+	BufferHandler(std::string name, std::string type, int total, int block_size) {
 		this->name = name;
 		this->total = total;
 		this->block_size = block_size;
 		this->type = type;
 	}
 
-	std::string fetch_file_name(int i){
+	std::string fetch_file_name(int i) {
 		std::stringstream ss;
 		ss << i;
 		std::string index = ss.str();
 		return name + index;
 	}
 
-	std::string merge(std::string file_one, std::string file_two){
+	std::string merge(std::string file_one, std::string file_two) {
 		first_buffer.open(file_one.c_str(), std::ios::in);
 		second_buffer.open(file_two.c_str(), std::ios::in);
 
 		unsigned int count = 0;
+		int one_flag, two_flag;
+		one_flag = two_flag = 1;
 		std::vector<std::string> file_one_row;
 		std::vector<std::string> file_two_row;
 		std::string filename, one_temp, two_temp;
-		// Working here need to merge the two files while obaying the 22 block limit.
-		File page = NULL;
-		while (this->first_buffer.is_open() || this->second_buffer.is_open()){
-			for (int i = 0; i)
-			if (this->first_buffer >> one_temp)
+
+		File *page = NULL;
+		while (std::getline(this->first_buffer, one_temp) || std::getline(this->second_buffer, two_temp)) {
+			if (this->first_buffer >> one_temp && one_flag)
 				read(file_one_row, this->first_buffer, one_temp);
 
-			if (this->second_buffer >> two_temp)
+			if (this->second_buffer >> two_temp && two_flag)
 				read(file_two_row, this->second_buffer, two_temp);
-			
 
+			if (page == NULL)
+				//page = new File(filename, true);
+
+				if (file_compare(file_one_row[0], file_two_row[0])) {
+					addToPage(page, file_one_row);
+					one_flag = 1;
+					two_flag = 0;
+				}
+				else {
+					addToPage(page, file_two_row);
+					one_flag = 0;
+					two_flag = 1;
+				}
+
+			count++;
+
+			if (count == block_size) {
+				//page.save();
+				delete page;
+				page = NULL;
+			}
 		}
 
 		first_buffer.close();
@@ -340,10 +386,10 @@ public:
 		return "temp";
 	}
 
-	void merge_files(){
+	void merge_files() {
 		if (total > 0)
 			return;
-	
+
 		std::string merged_file;
 		std::string first_file;
 		std::string second_file;
@@ -353,7 +399,7 @@ public:
 		merged_file = merge(first_file, second_file);
 
 		// Second Pass
-		for (int i = 2; i < total-1; i++){
+		for (int i = 2; i < total - 1; i++) {
 			second_file = fetch_file_name(i);
 			merged_file = merge(merged_file, second_file);
 		}
@@ -365,7 +411,7 @@ int main()
 	int memory_size = 22;
 	FileHandler dept_file("Dept.csv", "dept", memory_size);
 	FileHandler emp_file("Emp.csv", "emp", memory_size);
-	
+
 	int total_dept = dept_file.read_table();
 	int total_emp = emp_file.read_table();
 
